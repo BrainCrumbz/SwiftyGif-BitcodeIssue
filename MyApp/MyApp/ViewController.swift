831466fac2df3ef9af46171ca384640c20c455b3
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2019-05-28 11:27:18 +0200

    feat(repo): aggiungi workspace, progetto libreria e progetto client

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by BrainCrumbz on 28/05/19.
+//  Copyright © 2019 BrainCrumbz. All rights reserved.
+//
+
+import UIKit
+import MyLib
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let adder = Adder()
+        
+        let result = adder.add(a: 1, b: 2)
+        
+        print("result: \(result)")
+    }
+
+
+}
+
---
+++ MyLib/MyLib/Adder.swift
@@ -0,0 +1,20 @@
+//
+//  Adder.swift
+//  MyLib
+//
+//  Created by BrainCrumbz on 28/05/19.
+//  Copyright © 2019 BrainCrumbz. All rights reserved.
+//
+
+import Foundation
+
+public class Adder {
+    
+    public init() {
+        // do nothing
+    }
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
